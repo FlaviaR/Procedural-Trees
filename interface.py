ce7f92f2a1f2b68358275ee409e95b06b9b5edb7
Flavia Cavalcanti <flavia@cygnus.hitronhub.home>
2018-03-04 13:39:09 -0700

    update to interface - still looks horrible but is more usable

--- interface.py
+++ interface.py
@@ -19,16 +19,19 @@ class Example(QWidget):
 		
 	def initUI(self):
 		rulesTitle = QLabel('L-System Rules')
-		rules = QLabel("F - Move forward a step of length d\nf  - Move forward a step of length d without drawing a line\n+ - Turn left by angle a\n- - Turn right by angle a\n& - Pitch down by angle a\n^ - Pitch up by angle a\n\ - Roll left by angle a\n/ - Roll right by angle a\n| - Turn arund (180 deg)\n[ - Push current drawing to stack\n] - Pop current drawing from stack")
+		rules = QLabel("F - Move forward a step of length d\n+ - Turn left by angle a\n- - Turn right by angle a\n& - Pitch down by angle a\n^ - Pitch up by angle a\n\ - Roll left by angle a\n/ - Roll right by angle a\n| - Turn arund (180 deg)\n[ - Push current drawing to stack\n] - Pop current drawing from stack")
 
-		pre_Rules = QLabel('Preset Rules')
+		pre_Rules = QLabel('Pre-made Rules')
 		combo = QComboBox(self)
 		for key in self.ruleDict:
 			combo.addItem(key)
 		combo.activated[str].connect(self.onActivated)
 
 		own_Rules = QLabel('Make Your Own Rules')
-		self.rulesEdit = QLineEdit()
+		self.ownAngle = QLineEdit()
+		self.ownNum = QLineEdit()
+		self.ownSentence = QLineEdit()
+		self.ownRules = QLineEdit()
 		
 		spheres = QCheckBox('Add Spheres', self)
 		spheres.stateChanged.connect(self.setSpheres)
@@ -39,63 +42,68 @@ class Example(QWidget):
 		build = QPushButton('Build Tree!', self)
 		build.clicked.connect(self.on_click)
 
-		console = QLabel('CONSOLE')
-		consoleInfo = QLabel('')
-
 		grid = QGridLayout()
 		grid.setSpacing(10)
-		
-		grid.addWidget(rulesTitle, 1, 0)
-		grid.addWidget(rules, 1, 1)
 
-		grid.addWidget(pre_Rules, 2, 0)
-		grid.addWidget(combo, 2, 1)
+		grid.addWidget(pre_Rules, 3, 2)
+		grid.addWidget(combo, 4, 2)
 
-		grid.addWidget(own_Rules, 3, 0)
-		grid.addWidget(self.rulesEdit, 3,1)
+		grid.addWidget(own_Rules, 5, 2)
+		grid.addWidget(self.ownAngle, 6,2)
+		grid.addWidget(self.ownNum, 7,2)
+		grid.addWidget(self.ownSentence, 8,2)
+		grid.addWidget(self.ownRules, 9,2)
 
-		grid.addWidget(spheres, 4, 0)
-		grid.addWidget(rec, 4, 1)
-		grid.addWidget(build, 4, 2)
+		grid.addWidget(rulesTitle, 1, 3)
+		grid.addWidget(rules, 2, 3)
+		grid.addWidget(build, 3, 3)
 
-		grid.addWidget(console, 5, 0 )
-		grid.addWidget(consoleInfo, 5, 1)
+		grid.addWidget(spheres, 3, 4)
+		grid.addWidget(rec, 4, 4)
 
 		self.setLayout(grid)
 		
-		self.setGeometry(300, 300, 350, 300)
+#		self.setGeometry(300, 300, 350, 300)
 		self.setWindowTitle('Tree Builder')
 		self.show()
 
 	def onActivated(self, text):
 		self.func = self.ruleDict[text]
 
+	def buildOwnTree(self):
+		angle = self.ownAngle.text()
+		num = self.ownNum.text()
+		sentence = self.ownSentence.text()
+		rules = self.ownRules.text()
+		
+		if (angle is '' or num is '' or sentence is '' or rules is ''):
+			return None
+		else:
+			angle = float(angle)
+			num = int(num)
+
+			rules = rules.split(',')
+			d = {}
+			i = 0
+			while i < len(rules) - 1:
+				key = rules[i]
+				val = rules[i+1]
+				if key is not "" and val is not "":
+					d[key] = val
+				i += 2
+			return self.treeBuilder.rules.createCustomRule(angle, sentence, num, d)
+
 	def on_click(self):
 		rules = self.func
-		text = self.rulesEdit.text()
 		
 		if self.buildRec:
 			self.treeBuilder.draw(rec = True)
 			subprocess.call(["open", "BuildTree.scad"])
 			return
 		
-		if text is not '':
-			r = text.split(',')
-			a = float(r[0])
-			s = r[1]
-			n = int(r[2])
-			d = {}
-			i = 3
-			while i < len(r) - 1:
-				key = r[i]
-				val = r[i+1]
-				if key is not "" and val is not "":
-					d[key] = val
-
-				i += 2
-
-			rules = self.treeBuilder.rules.createCustomRule(a, s, n, d)
-			
+		if self.buildOwnTree() is not None:
+			rules = self.buildOwnTree()
+		
 		self.treeBuilder.useSpheres(self.addSphere)
 		self.treeBuilder.draw(rule = rules)
 		subprocess.call(["open", "BuildTree.scad"])
